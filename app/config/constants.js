4386813c2da0fc74e8904bed55e1e6f845598b25
Avnyr Tokirawa <mathis.herriot@outlook.fr>
2021-12-19 00:20:48 +0100

    distribution.json update

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,5 @@
 const APP_DATA_NAME = 'IngridLauncher';
-const LIVE_DISTRIBUTION_URL = 'https://adlc.network/deploy/distribution.json';
-const DEV_DISTRIBUTION_URL = 'https://adlc.network/dev-deploy/distribution.json';
+const LIVE_DISTRIBUTION_URL = 'https://raw.githubusercontent.com/AdlC-Network/Ingrid/master/docs/distribution.json';
+const DEV_DISTRIBUTION_URL = 'https://raw.githubusercontent.com/AdlC-Network/Ingrid/master/docs/dev-distribution.json';
 
 module.exports = { APP_DATA_NAME, LIVE_DISTRIBUTION_URL, DEV_DISTRIBUTION_URL };
