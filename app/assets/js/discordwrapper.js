3b3f70b382a89f8c7a9a86aa6cd804862faae1b8
Avnyr Tokirawa <mathis.herriot@outlook.fr>
2021-12-27 21:17:54 +0100

    Semicolon fix

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -1,14 +1,14 @@
 // Work in progress
-const logger = require('./loggerutil')('%c[DiscordWrapper]', 'color: #7289da; font-weight: bold')
+const logger = require('./loggerutil')('%c[DiscordWrapper]', 'color: #7289da; font-weight: bold');
 
-const {Client} = require('discord-rpc')
+const {Client} = require('discord-rpc');
 
-let client
-let activity
+let client;
+let activity;
 
-exports.initRPC = function(genSettings, servSettings = null, initialDetails = 'Waiting for Client..'){
-    logger.log('Now Loading Discord RPC')
-    client = new Client({ transport: 'ipc' })
+exports.initRPC = function(genSettings, servSettings = null, initialDetails = 'En attente du client..'){
+    logger.log('Now Loading Discord RPC');
+    client = new Client({ transport: 'ipc' });
 
     activity = {
         details: initialDetails,
@@ -16,29 +16,29 @@ exports.initRPC = function(genSettings, servSettings = null, initialDetails = 'W
         largeImageText: genSettings.smallImageText,
         startTimestamp: new Date().getTime(),
         instance: false
-    }
+    };
 
     client.on('ready', () => {
-        logger.log('Discord RPC Connected')
-        client.setActivity(activity)
-    })
+        logger.log('Discord RPC Connected');
+        client.setActivity(activity);
+    });
     
     client.login({clientId: genSettings.clientId}).catch(error => {
         if(error.message.includes('ENOENT')) {
-            logger.log('Unable to initialize Discord Rich Presence, no client detected.')
+            logger.log('Unable to initialize Discord Rich Presence, no client detected.');
         } else {
-            logger.log('Unable to initialize Discord Rich Presence: ' + error.message, error)
-        }l
-    })
-}
+            logger.log('Unable to initialize Discord Rich Presence: ' + error.message, error);
+        }
+    });
+};
 
 exports.updateState = function(state){
     if(client){
-        activity.state = state
-        client.setActivity(activity)
-        logger.log('Updated discord state to: ' + state)
+        activity.state = state;
+        client.setActivity(activity);
+        logger.log('Updated discord state to: ' + state);
     }
-}
+};
 
 exports.clearState = function(){
     if(client){
@@ -48,19 +48,19 @@ exports.clearState = function(){
             largeImageText: activity.largeImageText,
             startTimestamp: activity.startTimestamp,
             instance: activity.instance
-        }
-        client.setActivity(activity)
-        logger.log('Cleared the activity state!')
+        };
+        client.setActivity(activity);
+        logger.log('Cleared the activity state!');
     }
-}
+};
 
 exports.updateDetails = function(details){
     if(client){
-        activity.details = details
-        client.setActivity(activity)
-        logger.log('Updated discord details to: ' + details)
+        activity.details = details;
+        client.setActivity(activity);
+        logger.log('Updated discord details to: ' + details);
     }
-}
+};
 
 exports.clearDetails = function(){
     if(client){
@@ -70,27 +70,27 @@ exports.clearDetails = function(){
             largeImageText: activity.largeImageText,
             startTimestamp: activity.startTimestamp,
             instance: activity.instance
-        }
-        logger.log('Cleared the activity details!')
+        };
+        logger.log('Cleared the activity details!');
     }
-}
+};
 
 exports.resetTime = function(){
     if(client){
-        activity.startTimestamp = new Date().getTime()
-        client.setActivity(activity)
-        logger.log('Reset the activity time!')
+        activity.startTimestamp = new Date().getTime();
+        client.setActivity(activity);
+        logger.log('Reset the activity time!');
     }
-}
+};
 
 exports.shutdownRPC = function(){
-    if(!client) return
-    client.clearActivity()
-    client.destroy()
-    client = null
-    activity = null
-}
+    if(!client) return;
+    client.clearActivity();
+    client.destroy();
+    client = null;
+    activity = null;
+};
 
 exports.getClient = function(){
-    return client
-}
+    return client;
+};
